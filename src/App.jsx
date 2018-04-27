bceecbaa70c1b079e975e94481cc838349c32d8e
Steven <stevenhuynh17@gmail.com>
2018-04-27 14:55:28 -0700

    feat: capable of adding new contacts

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 import ListContacts from './ListContacts.jsx'
 import * as ContactsAPI from './utils/ContactsAPI'
 import CreateContact from './CreateContact.jsx'
+import { Route } from 'react-router-dom'
 
 class App extends Component {
   state = {
     contacts: [],
-    screen: 'list'
   }
 
   componentDidMount() {
@@ -28,19 +28,32 @@ class App extends Component {
     ContactsAPI.remove(contact)
   }
 
+  createContact = (contact) => {
+    ContactsAPI.create(contact)
+      .then((contact) => {
+        this.setState((currentState) => ({
+          contacts: currentState.contacts.concat([contact])
+        }))
+      })
+  }
+
   render() {
     return (
       <div className="App">
-        {this.state.screen === 'list' && (
+        <Route exact path='/' render={() => (
           <ListContacts
             contacts={this.state.contacts}
             onDeleteContact={this.removeContact}
           />
-        )}
-        {this.state.screen === 'create' && (
-          <CreateContact />
-        )}
-
+        )}/>
+      <Route path='/create' render={({history}) => (
+            <CreateContact
+              onCreateContact={(contact) => {
+                this.createContact(contact)
+                history.push('/')
+              }}
+            />
+        )}/>
       </div>
     );
   }
